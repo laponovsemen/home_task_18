1393e56837db64854c229458308ee2a5a47ad32f
Laponov Semen <johndoe@example.com>
2023-06-28 09:38:09 +0300

    fix mistake with date format in blog creation

--- src/users/users.reposiroty.ts
+++ src/users/users.reposiroty.ts
@@ -84,7 +84,7 @@ export class UsersRepository {
   }
 
   async createUser(DTO: any) {
-    const dateOfCreation = new Date().toISOString()
+    const dateOfCreation = new Date()
     const login = DTO.login
     const password = DTO.password
     const email = DTO.email
