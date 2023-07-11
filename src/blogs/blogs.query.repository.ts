b185bedc5035be5b60ce668971471085cdd14f0b
Laponov Semen <johndoe@example.com>
2023-07-11 09:18:00 +0300

    start migration on typeorm

--- src/blogs/blogs.query.repository.ts
+++ src/blogs/blogs.query.repository.ts
@@ -315,14 +315,14 @@ export class BlogsQueryRepository {
         .getRepository(Blog)
         .createQueryBuilder("blog")
         .leftJoinAndSelect('blog.blogOwner', 'blogOwner')
-        .where('id = :blogId', { blogId })
+        .where('blog.id = :blogId', { blogId })
         .getSql()
 
     const foundBlog = await this.dataSource
         .getRepository(Blog)
         .createQueryBuilder("blog")
         .leftJoinAndSelect('blog.blogOwner', 'blogOwner')
-        .where('id = :blogId', { blogId })
+        .where('blog.id = :blogId', { blogId })
         .getOne()
 
 
